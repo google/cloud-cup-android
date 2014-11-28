19ce005d9e77989cbc827467e547bacbbd5d1366
Steren Giannini <steren@google.com>
2014-11-28 10:17:26 +0100

    change firebase URL

--- app/src/main/java/com/example/mirnabouchra/cloudolympics/Consts.java
+++ app/src/main/java/com/example/mirnabouchra/cloudolympics/Consts.java
@@ -4,6 +4,6 @@ package com.example.mirnabouchra.cloudolympics;
  * Created by steren on 11/18/14.
  */
 public class Consts {
-    public static final String FIREBASE_URL = "https://cloud-olympics.firebaseio.com";
+    public static final String FIREBASE_URL = "https://cloud-cup.firebaseio.com";
 }
 
