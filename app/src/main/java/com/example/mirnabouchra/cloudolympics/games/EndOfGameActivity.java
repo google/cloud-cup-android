fc1421dbc1c9d18774566e23c0c9fd26dffff925
Steren Giannini <steren@google.com>
2014-11-28 11:18:49 +0100

    reset game state and game number to initial state when hitting replay

--- app/src/main/java/com/example/mirnabouchra/cloudolympics/games/EndOfGameActivity.java
+++ app/src/main/java/com/example/mirnabouchra/cloudolympics/games/EndOfGameActivity.java
@@ -12,6 +12,9 @@ import com.firebase.client.Firebase;
 public class EndOfGameActivity extends GameActivity {
     private static final String LOG_TAG = EndOfGameActivity.class.getSimpleName();
 
+    private Firebase stateRef;
+    private Firebase currentGameRef;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         state = GameState.DONE;
@@ -19,10 +22,18 @@ public class EndOfGameActivity extends GameActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.end_of_game);
 
+        stateRef = new Firebase(Consts.FIREBASE_URL + "/room/" + code + "/state");
+        currentGameRef = new Firebase(Consts.FIREBASE_URL + "/room/" + code + "/currentGame");
+
+
         final ImageButton button = (ImageButton) findViewById(R.id.startButton);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                // we should be at "done
+                stateRef.setValue("waiting");
+                currentGameRef.setValue(-1);
 
+                // TODO, change the player scores to 0
             }
         });
 
